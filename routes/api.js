06ec507e74403b6ed7d2af4ad5d9fb7b3d9f24db
split750 <godefroy.lemerdy@gmail.com>
2015-11-21 00:30:18 +0100

    add vcards-js

--- routes/api.js
+++ routes/api.js
@@ -186,4 +186,28 @@ router.route('/profil/:id')
 		}
 	});
 
+
+router.route('/profil/:id/vcard')
+	
+	.get(function(req, res) {
+		var vCard = require('vcards-js');
+
+		//create a new vCard
+	    vCard = vCard();
+
+	    //set properties
+	    vCard.firstName = 'Eric';
+	    vCard.middleName = 'J';
+	    vCard.lastName = 'Nesser';
+	    vCard.organization = 'ACME Corporation';
+
+	    //set content-type and disposition including desired filename
+	    res.set('Content-Type', 'text/vcard; name="enesser.vcf"');
+	    res.set('Content-Disposition', 'inline; filename="enesser.vcf"');
+
+	    //send the response
+	    res.send(vCard.getFormattedString());
+	});
+
+
 module.exports = router;
